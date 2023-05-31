dda6294786567edece0fa4ee7c4037ea62680df5
parkjei <parkjei94@gmail.com>
2023-05-31 13:58:31 +0900

    검색 기능 추가

--- static/main.js
+++ static/main.js
@@ -27,3 +27,37 @@ window.addEventListener("load", async () => {
     `;
   });
 });
+
+searchBtn.addEventListener("click", async () => {
+  if (!searchIp.value) return alert("영화 제목을 입력해주세요");
+  // 빈 값인 상태에서 검색 버튼 클릭 시 알럿 노출
+
+  const api = await fetch("https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1", {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYTk2ZGJlN2Y4M2EyOTlhNDNkYTY1OTU3Y2U3YzFkYyIsInN1YiI6IjY0NzBiYjAwNzI2ZmIxMDE0NGU2MjgwZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.xX1FH_36KZ8QkDBdbs8u5Bl3YZt32lyDNMGaNLcVz5E",
+    },
+  });
+  const { results } = await api.json();
+  const searchResult = results.filter((val) => val["title"].toUpperCase().indexOf(searchIp.value.toUpperCase()) !== -1);
+  /* 영화 제목 데이터(대문자 변환)에 검색창에 입력된 값(대문자 변환)이 포함되어 있는지 확인하고, 포함되어 있을 경우 true가 반환되어 해당 요소는 필터링된 배열에 포함
+       포함되지 않았을 경우 flse를 반환하고 요소는 필터링된 배열에서 제외됨 */
+  if (searchResult.lenghth == 0) return alert("검색어에 일치하는 영화가 없습니다.");
+  /* 검색어에 일치하는 영화가 없을 경우 알럿 노출 */
+
+  movieList.innerHTML = "";
+  searchResult.forEach((result) => {
+    movieList.innerHTML += `<div onclick="alert('영화 ID : ${result.id}')" class="movie-box">
+    <div class="card h-100 cards">
+    <img src="https://image.tmdb.org/t/p/original/${result["poster_path"]}" class="card-img-top" />
+    <div class="card-body">
+      <h5 class="card-title">${result.title}</h5>
+      <p>Rating : ${result["vote_average"]}</p>
+      <p class="card-text">${result.overview}</p>
+    </div>
+</div>
+    `;
+  });
+});
+// onclick="alert() : 클릭 시 알럿 노출
